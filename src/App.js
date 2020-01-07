6adf78ac20669e15a268e4927038ccea4b9a685b
nikita-seliverstov <fubirino@gmail.com>
2020-01-07 23:51:37 +0200

    add img, bootstrap,

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "./App.css";
+import Img3xBAR from "./images/3xBAR.png";
+import Img2xBAR from "./images/2xBAR.png";
+import ImgBAR from "./images/BAR.png";
+import Img7 from "./images/7.png";
+import ImgCherry from "./images/Cherry.png";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="container">
+        <div className="container-fluid">
+        <div className="row">
           <div className="col">
+            
             <Reel />
+            </div>
+            <div className="col">
+            <Reel />
+            </div>
+            <div className="col">
+            <Reel />
+            </div>
           </div>
+          
         </div>
       </header>
     </div>
@@ -18,12 +34,11 @@ function Reel() {
   return (
     <div class="scene">
       <div class="carousel">
-        <div class="carousel__cell">1</div>
-        <div class="carousel__cell">2</div>
-        <div class="carousel__cell">3</div>
-        <div class="carousel__cell">4</div>
-        <div class="carousel__cell">5</div>
-  
+        <div class="carousel__cell"><img className="slotImages"  src={Img3xBAR}></img></div>
+        <div class="carousel__cell"><img className="slotImages" src={ImgBAR}></img></div>
+        <div class="carousel__cell"><img className="slotImages" src={Img2xBAR}></img></div>
+        <div class="carousel__cell"><img className="slotImages" src={Img7}></img></div>
+        <div class="carousel__cell"><img className="slotImages" src={ImgCherry}></img></div>
       </div>
     </div>
   );
